10281495492ecbfbedd524f29ff8c9083e8afbcf
beatriz braga <biabbraga@gmail.com>
2025-02-26 17:09:15 -0300

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Restaurant.destroy_all
+
+restaurants = [
+
+  {
+    name:     "Szechuan Spice",
+    address:  "123 Peppercorn Rd, Shanghai",
+    category: "chinese"
+  },
+
+  {
+    name:     "Mamma Mia",
+    address:  "456 Roma Ave, Rome",
+    category: "italian"
+  },
+
+  {
+    name:     "Samurai Sushi",
+    address:  "789 Sakura Ln, Tokyo",
+    category: "japanese"
+  },
+
+  {
+    name:     "Le Gourmet",
+    address:  "101 Rue de la Paix, Paris",
+    category: "french"
+  },
+
+  {
+    name:     "Waffle House",
+    address:  "11 Grande Place, Brussels",
+    category: "belgian"
+  }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
